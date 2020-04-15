21344dd9847e0431f2790cf2e564f2bf4b7168c4
Joan Yassi <joan@tray.io>
2020-04-15 03:57:37 +0100

    Added the api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card } from './components';
+import { fetchData } from './api';
 import styles from './styles.module.css';
 
 const App = () => {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const getData = async () => {
+      const data = await fetchData();
+      setData(data);
+    };
+
+    getData();
+  }, []);
+
   return (
     <div className={styles.container}>
-      <h1>Hello from App</h1>
+      {console.log('data', data)}
+      <h1>Covid-19 Tracker</h1>
       <section className={styles.cardsContainer}>
-        <Card dataColor='primary' />
-        <Card dataColor='secondary' />
-        <Card dataColor='tertiary' />
+        {data && data.lastUpdate ? (
+          <Card
+            dataColor='primary'
+            data={data.confirmed}
+            lastUpdate={data.lastUpdate}
+            title='Confirmed'
+          />
+        ) : null}
+        {data && data.lastUpdate ? (
+          <Card
+            dataColor='secondary'
+            data={data.recovered}
+            lastUpdate={data.lastUpdate}
+            title='Recovered'
+          />
+        ) : null}
+        {data && data.lastUpdate ? (
+          <Card
+            dataColor='tertiary'
+            data={data.deaths}
+            lastUpdate={data.lastUpdate}
+            title='Deaths'
+          />
+        ) : null}
       </section>
     </div>
   );
